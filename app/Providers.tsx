367be334d2bc3aa689e0fdc08bc5c49f03476638
yusuf durmaz <durmazyusuf421@gmail.com>
2026-04-09 21:33:32 +0300

    fatura yazdir: window.open+print geri getirildi, yazdir sayfasi silindi, Providers temizlendi

--- app/Providers.tsx
+++ app/Providers.tsx
@@ -1,15 +1,11 @@
 "use client";
-import { usePathname } from "next/navigation";
 import { ToastProvider } from "@/app/lib/toast";
 import AppWrapper from "@/app/AppWrapper";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-  const pathname = usePathname();
-  const isYazdir = pathname?.startsWith("/faturalar/yazdir");
-
   return (
     <ToastProvider>
-      {isYazdir ? children : <AppWrapper>{children}</AppWrapper>}
+      <AppWrapper>{children}</AppWrapper>
     </ToastProvider>
   );
 }
